4ab398ad45e41f0ab3b99271b26f4781d7801a3c
Leander Stephen <lestepbusiness@gmail.com>
2025-02-08 16:22:29 -0500

    changes

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Spotify API credentials
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+
+# OpenCV model paths
+FACE_PROTO = "models/opencv_face_detector.pbtxt"
+FACE_MODEL = "models/opencv_face_detector_uint8.pb"
+EMOTION_MODEL = "models/emotion_detection_model.pb"
